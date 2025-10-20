5577e8a9a5825eaa6f871439e8e076f65e88bccd
Shevchenko Daniil <shevchenko.daniil.alekseevich@gmail.com>
2025-10-20 22:38:49 +0600

    [fix] Исправлено имя рабочего процесса сборки и добавлен третий обработчик в main.go

--- main.go
+++ main.go
@@ -13,8 +13,19 @@ func secondHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "This is the second handler!")
 }
 
+func sum(a, b int) string {
+	result := fmt.Sprint(a + b)
+	return fmt.Sprint("Result is: %n", result)
+}
+
+func thirdHandler(w http.ResponseWriter, r *http.Request) {
+	message := sum(10, 10)
+	fmt.Fprintln(w, message)
+}
+
 func main() {
 	http.HandleFunc("/", helloHandler)
 	http.HandleFunc("/second", secondHandler)
+	http.HandleFunc("/third", thirdHandler)
 	http.ListenAndServe(":8080", nil)
 }
